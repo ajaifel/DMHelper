604eaf5f2be2b582a944a9c4164b1d4acf0d1419
Jack Baxter <jackbaxter007@btinternet.com>
2019-05-24 01:04:22 +0100

    First Test

--- app/src/main/java/com/btinternet/jackbaxter007/dmhelper/MainActivity.java
+++ app/src/main/java/com/btinternet/jackbaxter007/dmhelper/MainActivity.java
@@ -6,10 +6,109 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.AdvertisingOptions;
+import com.google.android.gms.nearby.connection.ConnectionInfo;
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
+import com.google.android.gms.nearby.connection.ConnectionResolution;
+import com.google.android.gms.nearby.connection.ConnectionsClient;
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
+import com.google.android.gms.nearby.connection.DiscoveryOptions;
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate.Status;
+import com.google.android.gms.nearby.connection.Strategy;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final Strategy STRATEGY = Strategy.P2P_STAR;
+    private static final String TAG = "DMHelper";
+    private ConnectionsClient connectionsClient;
+
+    private Button advertise;
+    private Button discover;
+    private Button connect;
+    private Button sendPayload;
+
+    private String endpointID;
+    private String recievedText;
+    private String partnerEndpointId;
+
     private TextView mTextMessage;
+
+    // Callbacks for receiving payloads
+    private final PayloadCallback payloadCallback =
+            new PayloadCallback() {
+                @Override
+                public void onPayloadReceived(String endpointId, Payload payload) {
+                    recievedText = new String(payload.asBytes());
+                    Toast.makeText(getApplicationContext(),recievedText, Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onPayloadTransferUpdate(String endpointId, PayloadTransferUpdate update) {
+                    recievedText = new String(update.toString());
+                    Toast.makeText(getApplicationContext(),recievedText, Toast.LENGTH_LONG).show();
+                }
+            };
+
+    // Callbacks for finding other devices
+    private final EndpointDiscoveryCallback endpointDiscoveryCallback =
+            new EndpointDiscoveryCallback() {
+                @Override
+                public void onEndpointFound(String endpointId, DiscoveredEndpointInfo info) {
+                    Log.i(TAG, "onEndpointFound: endpoint found, connecting");
+                    Toast.makeText(getApplicationContext(),"onEndpointFound: endpoint found, connecting", Toast.LENGTH_LONG).show();
+                    connectionsClient.requestConnection("WHY", endpointId, connectionLifecycleCallback);
+                }
+
+                @Override
+                public void onEndpointLost(String endpointId) {}
+            };
+
+    // Callbacks for connections to other devices
+    private final ConnectionLifecycleCallback connectionLifecycleCallback =
+            new ConnectionLifecycleCallback() {
+                @Override
+                public void onConnectionInitiated(String endpointId, ConnectionInfo connectionInfo) {
+                    Log.i(TAG, "onConnectionInitiated: accepting connection");
+                    Toast.makeText(getApplicationContext(),"onConnectionInitiated: accepting connection", Toast.LENGTH_LONG).show();
+                    connectionsClient.acceptConnection(endpointId, payloadCallback);
+                    //opponentName = connectionInfo.getEndpointName();
+                }
+
+                @Override
+                public void onConnectionResult(String endpointId, ConnectionResolution result) {
+                    if (result.getStatus().isSuccess()) {
+                        Log.i(TAG, "onConnectionResult: connection successful");
+                        Toast.makeText(getApplicationContext(),"onConnectionResult: connection successful", Toast.LENGTH_LONG).show();
+
+                        connectionsClient.stopDiscovery();
+                        connectionsClient.stopAdvertising();
+
+                        partnerEndpointId = endpointId;
+                    } else {
+                        Log.i(TAG, "onConnectionResult: connection failed");
+                        Toast.makeText(getApplicationContext(),"onConnectionResult: connection failed", Toast.LENGTH_LONG).show();
+                    }
+                }
+
+                @Override
+                public void onDisconnected(String endpointId) {
+                    Log.i(TAG, "onDisconnected: disconnected from partner");
+                    Toast.makeText(getApplicationContext(),"onDisconnected: disconnected from partner", Toast.LENGTH_LONG).show();
+                }
+            };
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -36,9 +135,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        advertise = findViewById(R.id.advertise);
+        discover = findViewById(R.id.discover);
+        connect = findViewById(R.id.connect);
+        sendPayload = findViewById(R.id.send);
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        connectionsClient = Nearby.getConnectionsClient(this);
     }
 
+    @Override
+    protected void onStop() {
+        connectionsClient.stopAllEndpoints();
+
+        super.onStop();
+    }
+
+    private void startDiscovery() {
+        // Note: Discovery may fail. To keep this demo simple, we don't handle failures.
+        connectionsClient.startDiscovery(
+                getPackageName(), endpointDiscoveryCallback,
+                new DiscoveryOptions.Builder().setStrategy(STRATEGY).build());
+    }
+
+    /** Broadcasts our presence using Nearby Connections so other players can find us. */
+    private void startAdvertising() {
+        // Note: Advertising may fail. To keep this demo simple, we don't handle failures.
+        connectionsClient.startAdvertising(
+                "WHY", getPackageName(), connectionLifecycleCallback,
+                new AdvertisingOptions.Builder().setStrategy(STRATEGY).build());
+    }
+
+    public void sendData (View view){
+        connectionsClient.sendPayload(
+                partnerEndpointId, Payload.fromBytes(recievedText.getBytes()));
+    }
+
+    public void mAdvertise(View view) {
+        startAdvertising();
+    }
+
+    public void mDiscover(View view) {
+        startDiscovery();
+    }
 }
